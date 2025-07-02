6934cadc3749a71f61a7af57ed43b49562f0d50f
beatrizglima <141534313+beatrizglima@users.noreply.github.com>
2025-07-02 18:40:10 -0300

    Formulário de investimentos e arquivo de cálculo de projeções criados

---
+++ src/investimentGoals.js
@@ -0,0 +1,58 @@
+function convertToMonthlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = 'monthly'
+) {
+  if (!timeHorizon || !startingAmount) {
+    // quando os valores forem 0 aparecerá um aviso de erro
+    throw new Error(
+      'Investimento inicial e prazo devem ser preenchidos com valores positivos.'
+    );
+  }
+  // converter para trabalhar na base mensal
+  const finalReturnRate =
+    returnTimeFrame === 'monthly'
+      ? 1 + returnRate / 100
+      : convertToMonthlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestimentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0, // rendimento do último mês
+    totalInterestReturns: 0, // rendimento total
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestimentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+    const interestReturns =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount; // tudo que se tem + o que investiu
+    returnsArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+  return returnsArray;
+}
